86c39ce8504b36d1e12ac449f6234cb6ed7bff75
Pausa <pausatep@gmail.com>
2015-10-26 20:00:00 +0100

    --added licence

--- app/src/main/java/com/android/madpausa/cardnotificationviewer/SettingsActivity.java
+++ app/src/main/java/com/android/madpausa/cardnotificationviewer/SettingsActivity.java
@@ -1,3 +1,22 @@
+/**
+ *  Copyright 2015 Antonio Petrella
+ *
+ *  This file is part of Card Notification Viewer
+ *
+ *   Card Notification Viewer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Card Notification Viewer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Card Notification Viewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.android.madpausa.cardnotificationviewer;
 
 import android.os.Bundle;
